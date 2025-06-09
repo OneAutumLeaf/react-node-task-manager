9303df4f563d280eca606c9e316b7cf06d6c2fa6
OneAutumLeafe <ayushsingh12072000@gmaile.com>
2025-06-09 13:08:14 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const corsOptions = {
+  origin: ['http://localhost:3000', 'https://your-frontend-app-name.vercel.app'] 
+};
+
 app.use(cors());
 app.use(express.json());
 
@@ -12,24 +16,17 @@ const EXTERNAL_API_URL = "http://122.163.123.122/CRM/Task/Task.svc/Task_List";
 
 app.get("/api/tasks", async (req, res) => {
   try {
-    console.log("Fetching all tasks from external API...");
+    // console.log("Fetching all tasks from external API...");
 
     const response = await axios.get(EXTERNAL_API_URL);
 
-    res.json(response.data);
+    res.json({ Task_listResult: response.data.Task_listResult || [] });
   } catch (error) {
     console.error("Error fetching data from external API:", error.message);
-    if (error.response) {
-      res.status(error.response.status).json({
-        message: "Error from external API",
-        details: error.response.data,
-      });
-    } else {
-      res.status(500).json({ message: "Internal Server Error" });
-    }
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend server is running on http://localhost:${PORT}`);
+  console.log(`Backend server is running on port ${PORT}`);
 });
